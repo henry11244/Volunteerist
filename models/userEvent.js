a8e0961394416d4d1a04c46520bac7557d892a5d
pbarkley <42686983+pbarkley@users.noreply.github.com>
2022-08-04 13:19:05 -0500

    create userEvent model

---
+++ models/userEvent.js
@@ -0,0 +1,39 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class userEvent extends Model {}
+
+userEvent.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        },
+        event_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'event',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        modelName: 'userEvent'
+    }
+);
+
+module.exports = userEvent;
