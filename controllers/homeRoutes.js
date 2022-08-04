e09db8f23e75d16999d1be24d209d63fbd926238
lilys tacos <lilyannek@gmail.com>
2022-08-04 11:33:03 -0700

    get routes for all events and events by location

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -11,4 +11,19 @@ router.get("/", async (req, res) => {
   }
 });
 
+// GET route for events by location
+router.get("/:location", async (req, res) => {
+  try {
+    eventData = await Event.findAll({
+      where: {
+        eventId: req.params.location,
+      },
+    });
+  } catch (err) {
+    res
+      .status(400)
+      .json({ message: "No events were found in this location! " });
+  }
+});
+
 module.exports = router;
