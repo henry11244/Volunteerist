2e538a92e99bb8683e3d9a9756ef0bf4868c55b5
lilys tacos <lilyannek@gmail.com>
2022-08-04 11:27:41 -0700

    withAuth

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,8 @@
 const router = require("express").Router();
 const userRoutes = require("./userRoutes.js");
 const dashboardRoutes = require("./dashboardRoutes.js");
-const eventRoutes = require("./eventRoutes.js");
 
 router.use("/users", userRoutes);
 router.use("/myevents", dashboardRoutes);
-router.use("/event", eventRoutes);
 
 module.exports = router;
