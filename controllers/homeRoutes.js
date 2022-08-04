2f543e2daa1ff399cf2b36723cdefbb07e02e97d
lilys tacos <lilyannek@gmail.com>
2022-08-04 12:05:54 -0700

    refactoring of home routes plus addition of event routes by location and category

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,9 +2,9 @@ const router = require("express").Router();
 const { Event } = require("../models");
 
 // GET route for all events
-router.get("/", async (req, res) => {
+router.get("/", withAuth, async (req, res) => {
   try {
-    eventData = await Event.findAll();
+    const eventData = await Event.findAll();
     res.json(eventData);
   } catch (err) {
     res.status(400).json(err);
@@ -12,17 +12,44 @@ router.get("/", async (req, res) => {
 });
 
 // GET route for events by location
-router.get("/:location", async (req, res) => {
+router.get("/:location", withAuth, async (req, res) => {
+  try {
+    const eventData = await Event.findAll({
+      where: {
+        eventLocation: req.params.location,
+      },
+    });
+
+    if (!eventData) {
+      res
+        .status(400)
+        .json({ message: "No events were found in this location!" });
+      return;
+    }
+
+    res.status(200).json(eventData);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// GET route for events by category
+router.get("/:category", withAuth, async (req, res) => {
   try {
     eventData = await Event.findAll({
       where: {
-        eventId: req.params.location,
+        eventCategory: req.params.location,
       },
     });
+
+    if (!eventData) {
+      res
+        .status(400)
+        .json({ message: "No events were found in this category!" });
+      return;
+    }
   } catch (err) {
-    res
-      .status(400)
-      .json({ message: "No events were found in this location! " });
+    console.log(err);
   }
 });
 
