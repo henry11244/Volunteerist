67894101d87cacb10232909fc3c19a1e495894db
Lily Kot <64398181+lilyannekot@users.noreply.github.com>
2022-08-05 10:51:40 -0700

    Update userRoutes.js

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,1 +1,79 @@
+const router = require("express").Router();
+const { User } = require("../../models");
+
+// POST route to create a new user
+router.post("/", (req, res) => {
+  User.create({
+    username: req.body.username,
+    password: req.body.password,
+  })
+    .then((userData) => {
+      req.sessionStore.save(() => {
+        req.session.userId = userData.id;
+        req.session.username = userData.username;
+        req.session.loggedIn = true;
+
+        res.json(userData);
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// POST route to find a user by login info and check password
+router.post("/login", (req, res) => {
+  User.findOne({
+    where: {
+      username: req.body.username,
+    },
+  });
+  if (!userData) {
+    res.status(400).json({ message: "No account could be found!" });
+    return;
+  }
+  const validPassword = userData.checkPassword(req.body.password);
+
+  if (!validPassword) {
+    res.status(400).json({ message: "Password isn't valid!" });
+    return;
+  }
+  req.sessionStore.save(() => {
+    req.session.userId = userData.id;
+    req.session.username = userData.username;
+    req.session.loggedIn = true;
+  });
+});
+
+// POST route for user logout
+router.post("/logout", (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  }
+});
+
+// POST route to delete user
+router.delete("/user/:id", (req, res) => {
+  User.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((userData) => {
+      if (!userData) {
+        res.status(404).json({ message: "No user could be found!" });
+        return;
+      }
+      res.json(userData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
 
