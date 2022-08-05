ed5934f3cae226d8e7a2f935212b5df278ddc63c
pbarkley <42686983+pbarkley@users.noreply.github.com>
2022-08-05 14:37:23 -0500

    add listeners

--- public/js/script.js
+++ public/js/script.js
@@ -3,3 +3,10 @@ const dashboardEl = document.querySelector("#dashboard")
 const createEl = document.querySelector("#create")
 const logoutEl = document.querySelector("#logout")
 const loginEl = document.querySelector("#login")
+
+
+hompageEl.addEventListener("click", )
+dashboardEl.addEventListener("click", )
+createEl.addEventListener("click", )
+logoutEl.addEventListener("click", )
+loginEl.addEventListener("click", )
