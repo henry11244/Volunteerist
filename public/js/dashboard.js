4acc8a0683b11f344d4728711054229329e7cb2f
Henry Nguyen <hln11244@gmail.com>
2022-08-08 11:29:50 -0700

    added event delete

---
+++ public/js/dashboard.js
@@ -0,0 +1,21 @@
+console.log('test')
+
+const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+        const eventId = event.target.getAttribute('data-id');
+
+        const response = await fetch(`/api/event/${eventId}`, {
+            method: 'DELETE',
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert('Failed to delete event');
+        }
+    }
+};
+
+
+const deletebutton = document.querySelectorAll('.delete')
+deletebutton.forEach(button => { button.addEventListener('click', delButtonHandler) });
