360bbf16f36443d5e0d64638dfac745f16034283
lilys tacos <lilyannek@gmail.com>
2022-08-09 18:04:58 -0700

    redirect users to login page when they click logout

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,39 +1,35 @@
-const nameSignupEl = document.querySelector("#nameSignup")
-const userSignupEl = document.querySelector("#usernameSignup")
-const passSignupEl = document.querySelector("#passwordSignup")
-const signupBtn = document.querySelector("#sign-up")
-
+const nameSignupEl = document.querySelector("#nameSignup");
+const userSignupEl = document.querySelector("#usernameSignup");
+const passSignupEl = document.querySelector("#passwordSignup");
+const signupBtn = document.querySelector("#sign-up");
 
 const signupFormHandler = async (event) => {
-    event.preventDefault();
-    console.log('test')
-    const name = nameSignupEl.value.trim();
-    const username = userSignupEl.value.trim();
-    const password = passSignupEl.value.trim();
+  event.preventDefault();
+  console.log("test");
+  const name = nameSignupEl.value.trim();
+  const username = userSignupEl.value.trim();
+  const password = passSignupEl.value.trim();
 
-    if (username && name && password) {
-        console.log(JSON.stringify({ name, username, password }))
-        const response = await fetch('/api/user/signup', {
-            method: 'POST',
-            body: JSON.stringify({ name, username, password }),
-            headers: { 'Content-Type': 'application/json' },
-        });
+  if (username && name && password) {
+    console.log(JSON.stringify({ name, username, password }));
+    const response = await fetch("/api/user/signup", {
+      method: "POST",
+      body: JSON.stringify({ name, username, password }),
+      headers: { "Content-Type": "application/json" },
+    });
 
-        if (response.ok) {
-            document.location.replace('/');
-        } else {
-            alert(response.statusText);
-        }
-    } else if (!name) {
-        alert("You must have a name!")
-    } else if (!username) {
-        alert("Please enter a valid username")
-    } else if (!password) {
-        alert("Please enter a valid password")
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert(response.statusText);
     }
-
+  } else if (!name) {
+    alert("You must have a name!");
+  } else if (!username) {
+    alert("Please enter a valid username");
+  } else if (!password) {
+    alert("Please enter a valid password");
+  }
 };
 
-
-signupBtn.addEventListener("click", signupFormHandler)
-
+signupBtn.addEventListener("click", signupFormHandler);
